4fd1101812fa41bb4dfdaa85315f9b558fe9a25b
Isidro García García <isidro.gg@gmail.com>
2014-02-06 23:19:36 +0100

    Bootstrap

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,11 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new Frieser\WSSEAuthenticationBundle\FrieserWSSEAuthenticationBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle($this),
+            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Craue\FormFlowBundle\CraueFormFlowBundle(),
+
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
